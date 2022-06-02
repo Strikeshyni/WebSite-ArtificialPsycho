b3c34cfd561bf8f6bbe1f891d66ae28a11ebfd9d
Abel AUBRON <abel.aubron@epita.fr>
2022-06-02 19:22:01 +0200

    DB connection 11

--- database.php
+++ database.php
@@ -1,5 +1,6 @@
 <?php	
 	//Get Heroku ClearDB connection information
+	/*
 	$cleardb_server = "eu-cdbr-west-02.cleardb.net";
 	$cleardb_username = "be89b74cd405c2";
 	$cleardb_password = "2d67980f";
@@ -10,5 +11,5 @@
 		$database = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 	} catch(PDOExeption $e){
 		die('Erreur :'.$e->getMessage());
-	}
+	}*/
 ?>
