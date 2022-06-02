ba8df793d6d15ce54f21edf6ed13aa3996f2f250
Abel AUBRON <abel.aubron@epita.fr>
2022-06-02 15:14:43 +0200

    database setup

--- database.php
+++ database.php
@@ -1,10 +1,11 @@
 <?php	
-	define('HOST','localhost');
-	define('DB_Name','bloodless');
-	define('USER','root');
+	define('HOST','eu-cdbr-west-02.cleardb.net');
+	define('DB_Name','heroku_9abe1d6d35f5f58');
+	define('USER','be89b74cd405c2');
+	define('PASS','2d67980f');
 	
 	try{
-		$database = new PDO("mysql:host=" . HOST . ";dbname=" . DB_Name, USER);
+		$database = new PDO("mysql:host=" . HOST . ";dbname=" . DB_Name, USER, PASS);
 	} catch(PDOExeption $e){
 		die('Erreur :'.$e->getMessage());
 	}
