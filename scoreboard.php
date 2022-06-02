75071092946878ec3a7dafc671d6f2e66681201d
Abel AUBRON <abel.aubron@epita.fr>
2022-06-02 18:58:53 +0200

    DB connection 8

--- scoreboard.php
+++ scoreboard.php
@@ -2,7 +2,7 @@
 require 'class/class.Head.php';
 require 'class/class.Header.php';
 include 'database.php';
-global $database;
+//global $database;
 
 $myhead=new Head();
 $myhead->addhead('stylesheet','css/main.css','BloodLess Studio: Scoreboard','UTF-8',1);
@@ -11,6 +11,20 @@ echo $myhead->getaddhead();
 $myheader=new Header();
 $myheader->addheader(1);
 echo $myheader->getaddheader();
+
+//Get Heroku ClearDB connection information
+$cleardb_server = "eu-cdbr-west-02.cleardb.net";
+$cleardb_username = "be89b74cd405c2";
+$cleardb_password = "2d67980f";
+$cleardb_db = "heroku_9abe1d6d35f5f58";
+
+try{
+  // Connect to DB
+  $database = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+} catch(PDOExeption $e){
+  die('Erreur :'.$e->getMessage());
+}
+
 
 $querry = $database->query("SELECT * FROM `scoreboard` ORDER BY Time");
 
@@ -44,8 +58,8 @@ while ($line = $querry->fetch_assoc()){
     </tr>";
     $rank++;
 }
-?>
 
+?>
 </tbody>
 </table>
 </div>
