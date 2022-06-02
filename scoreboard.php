bfb0b7cea600a6bd06555130fb51ed97de24a0e6
Abel AUBRON <abel.aubron@epita.fr>
2022-06-02 16:16:50 +0200

    DB connection 4

--- scoreboard.php
+++ scoreboard.php
@@ -4,11 +4,6 @@ require 'class/class.Header.php';
 include 'database.php';
 private $database;
 
-$servername = 'eu-cdbr-west-02.cleardb.net';
-$dbname = 'heroku_9abe1d6d35f5f58';
-$username = 'be89b74cd405c2';
-$password = '2d67980f';
-
 $myhead=new Head();
 $myhead->addhead('stylesheet','css/main.css','BloodLess Studio: Scoreboard','UTF-8',1);
 echo $myhead->getaddhead();
@@ -17,12 +12,16 @@ $myheader=new Header();
 $myheader->addheader(1);
 echo $myheader->getaddheader();
 
-// Create connection
-$database = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($database->connect_error) {
-  die("Connection failed: " . $database->connect_error);
-} 
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 
 $querry = $database->query("SELECT * FROM scoreboard ORDER BY Time");
