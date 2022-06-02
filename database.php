b51e4c4c3cdf271912e64d7d8016790c85fa8a9a
Abel AUBRON <abel.aubron@epita.fr>
2022-06-02 15:54:52 +0200

    DB connection 2

--- database.php
+++ database.php
@@ -1,12 +1,16 @@
 <?php	
-	define('HOST','eu-cdbr-west-02.cleardb.net');
-	define('DB_Name','heroku_9abe1d6d35f5f58');
-	define('USER','be89b74cd405c2');
-	define('PASS','2d67980f');
+	$servername = 'eu-cdbr-west-02.cleardb.net';
+	$dbname = 'heroku_9abe1d6d35f5f58';
+	$username = 'be89b74cd405c2';
+	$password = '2d67980f';
 	
-	try{
-		$database = new PDO("mysql:host=" . HOST . ";dbname=" . DB_Name, USER, PASS);
-	} catch(PDOExeption $e){
-		die('Erreur :'.$e->getMessage());
+	try {
+	  $database = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+	  // set the PDO error mode to exception
+	  $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	  echo "Connected successfully";
+	} catch(PDOException $e) {
+	  echo "Connection failed: " . $e->getMessage();
 	}
+
 ?>
