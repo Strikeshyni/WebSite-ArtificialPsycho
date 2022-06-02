abad660d37348fb4dc3b40094146ed4eea7493f9
Abel AUBRON <abel.aubron@epita.fr>
2022-06-02 18:46:11 +0200

    DB connection 7 and last

--- database.php
+++ database.php
@@ -1,16 +1,14 @@
 <?php	
-	$servername = 'eu-cdbr-west-02.cleardb.net';
-	$dbname = 'heroku_9abe1d6d35f5f58';
-	$username = 'be89b74cd405c2';
-	$password = '2d67980f';
+	//Get Heroku ClearDB connection information
+	$cleardb_server = "eu-cdbr-west-02.cleardb.net";
+	$cleardb_username = "be89b74cd405c2";
+	$cleardb_password = "2d67980f";
+	$cleardb_db = "heroku_9abe1d6d35f5f58";
 	
-	try {
-	  $con = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-	  // set the PDO error mode to exception
-	  $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	  echo "Connected successfully";
-	} catch(PDOException $e) {
-	  echo "Connection failed: " . $e->getMessage();
+	try{
+		// Connect to DB
+		$database = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+	} catch(PDOExeption $e){
+		die('Erreur :'.$e->getMessage());
 	}
-
 ?>
--- scoreboard.php
+++ scoreboard.php
@@ -2,7 +2,7 @@
 require 'class/class.Head.php';
 require 'class/class.Header.php';
 include 'database.php';
-private $database;
+global $database;
 
 $myhead=new Head();
 $myhead->addhead('stylesheet','css/main.css','BloodLess Studio: Scoreboard','UTF-8',1);
@@ -12,20 +12,7 @@ $myheader=new Header();
 $myheader->addheader(1);
 echo $myheader->getaddheader();
 
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url("mysql://be89b74cd405c2:2d67980f@eu-cdbr-west-02.cleardb.net/heroku_9abe1d6d35f5f58?reconnect=true");
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$database = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-
-
-$querry = $database->query("SELECT * FROM scoreboard ORDER BY Time");
+$querry = $database->query("SELECT * FROM `scoreboard` ORDER BY Time");
 
 $rank = 1;
 
@@ -48,7 +35,7 @@ echo "
   </thead>
   <tbody>";
 
-while ($line = $querry->fetch()){
+while ($line = $querry->fetch_assoc()){
     echo"
       <tr>
       <th scope='row'>$rank</th>
