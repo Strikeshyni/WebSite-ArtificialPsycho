f76a15285ec82e3b750843f15e8c2c3136581941
Abel AUBRON <abel.aubron@epita.fr>
2022-06-03 17:12:18 +0200

    index

--- index.php
+++ index.php
@@ -13,13 +13,13 @@ echo $myheader->getaddheader();
 echo "
         <div style='margin-top:80px;'></div>
 
-        <div class='justify-content-around py-5 px-3 mx-3 mt-5 bg-light rounded'>
+        <div class='justify-content-around py-5 px-3 mx-3 mt-5 bg-secondary text-light rounded'>
             <h3 class='pl-2'>Le Studio</h3>
             <p>Bloodless Studio c'est formé dans le cadre d'un projet de groupe. Pour notre projet de second semestre nous avions à construire ensemble un projet sur Unity et codé en C#. Nous avons ainsi déssider ensemble de crée Bloodless Studio. Studio qui va débuter sa première création en février 2022. Artificial psycho est alors en développement et toute l'équipe se donne à fond dans le projet. </p>
         </div>
 
-        <div class='mx-3 px-5 py-2 bg-light rounded'><h3> Les Membres </h3></div>
-        <div class='mx-3 d-flex justify-content-around shadow-sm p-3 bg-light rounded'>
+        <div class='mx-3 px-5 py-2 text-light rounded'><h3> Les Membres </h3></div>
+        <div class='mx-3 d-flex justify-content-around text-light shadow-sm p-3 rounded'>
             <figure class='figure'>
                 <img src='images/pp2.jpg' class='figure-img img-fluid img-thumbnail rounded' alt='...'>
                 <figcaption class='figure-caption text-center'>Hugo FRANGIAMONE</figcaption>
