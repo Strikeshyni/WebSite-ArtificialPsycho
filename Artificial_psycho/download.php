77834afd4eef831538ccd0a801c12227b24e7e1a
Abel AUBRON <abel.aubron@epita.fr>
2022-06-03 11:27:53 +0200

    download 2

--- Artificial_psycho/download.php
+++ Artificial_psycho/download.php
@@ -18,16 +18,12 @@ echo $myheader->getaddheader();
   <div class='card-body py-4 bg-secondary h-100'>
     <h4 class='card-title'></h4>
     <p class='card-text'></p>
-    
-    <div class="mx-auto w-100"><img class="d-block rounded w-100" src="images/vaisseau.png"></div>
-    <div class="mx-auto w-75"><a type="button" href="Artificial_psycho/Artificial_psycho.php" class="btn btn-outline-dark btn-lg btn-block my-1 w-100 mx-auto">GO TO ARTIFICIAL PSYCHO</a></div>
-
   </div>
 </div>
 
 
 <div class='justify-content-around shadow-sm py-5 px-3 mx-3 mt-5 mb-1 bg-light rounded'>
-  <h1 class='display-5'>Download</h1>
+  <h1 class='display-5'>All Download</h1>
   <hr class='my-4'>
   <h3>Executable</h3>
   <p class='lead'>Vous pourrez bientôt télécharger ici les différentes versions du jeux.</p>
