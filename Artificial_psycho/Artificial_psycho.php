027d0df7469e141c3f68781cbbaf12efba88724c
Abel AUBRON <abel.aubron@epita.fr>
2022-06-03 15:08:25 +0200

    Padding

--- Artificial_psycho/Artificial_psycho.php
+++ Artificial_psycho/Artificial_psycho.php
@@ -12,10 +12,10 @@ echo $myheader->getaddheader();
 ?>
 <div class="mainartif" style="background-image: url('../images/couloir.png'); height: 100vh; background-size: cover;">
 
-<div class='mx-auto bg-dark text-white' style="padding-top: 70px;">
+<div class='mx-auto bg-dark text-white' style="padding-top: 100px;">
     <h1 class="text-center">Artificial psycho</h1>
 </div>
-<div class='mx-auto w-75 bg-dark text-white' style="padding-top: 30px;">
+<div class='mx-auto w-75 bg-dark text-white' style="padding-top: 40px;">
     <p>Vous êtes un robot envoyé pour aller éliminer d'étranges I.A qui infeste un bâtiment. Une fois votre vaisseau arrivé prêt du bâtiment en question, une voix, celle de votre vaisseau, vous explique que ces I.A sont victime d'un virus informatique qui les rendent violentes. Une fois ces I.A éliminées, votre vaisseau vous indique qu'un problème similaire a été déclaré un peu plus loin, vous décidez alors de partir explorer tout les lieux présentant la moindre trace de ces I.A. si mystérieusement dangereuses...</p>
 </div>
 </div>
