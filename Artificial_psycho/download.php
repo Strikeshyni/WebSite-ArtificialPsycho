42c87779b9bc4a211c0e6dc23c81458c5bde44b5
Abel AUBRON <abel.aubron@epita.fr>
2022-06-03 11:37:05 +0200

    download 3 rapport soutenance

--- Artificial_psycho/download.php
+++ Artificial_psycho/download.php
@@ -16,29 +16,32 @@ echo $myheader->getaddheader();
 <div class='card border border-dark mx-2 h-65'>
   <h5 class='card-header bg-dark text-light'>All Download</h5>
   <div class='card-body py-4 bg-secondary h-100'>
+
+  <h5 class="card-title">Soutenances</h5>
   <ol class="list-group list-group-numbered">
   <li class="list-group-item d-flex justify-content-between align-items-start">
     <div class="ms-2 me-auto">
-      <div class="fw-bold">Subheading</div>
-      Content for list item
+      <div class="fw-bold">Soutenance 1</div>
+      Rapport de première soutenance
     </div>
-    <span class="badge bg-primary rounded-pill">14</span>
+    <span class="badge bg-primary rounded-pill">Download PDF</span>
   </li>
   <li class="list-group-item d-flex justify-content-between align-items-start">
     <div class="ms-2 me-auto">
-      <div class="fw-bold">Subheading</div>
-      Content for list item
+      <div class="fw-bold">Soutenance 2</div>
+      Rapport de seconde soutenance
     </div>
-    <span class="badge bg-primary rounded-pill">14</span>
+    <span class="badge bg-primary rounded-pill">Download PDF</span>
   </li>
   <li class="list-group-item d-flex justify-content-between align-items-start">
     <div class="ms-2 me-auto">
-      <div class="fw-bold">Subheading</div>
-      Content for list item
+      <div class="fw-bold">Soutenance 3</div>
+      Rapport de troisième soutenance
     </div>
-    <span class="badge bg-primary rounded-pill">14</span>
+    <span class="badge bg-primary rounded-pill">Download PDF</span>
   </li>
-</ol>
+  </ol>
+
   </div>
 </div>
 </body>
