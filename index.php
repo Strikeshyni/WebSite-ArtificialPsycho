8546f2f8bfc0896c576cf928a88d6d35215f8f85
Abel AUBRON <abel.aubron@epita.fr>
2022-06-03 17:23:30 +0200

    index 4

--- index.php
+++ index.php
@@ -9,17 +9,17 @@ echo $myhead->getaddhead();
 $myheader=new Header(); 
 $myheader->addheader(1);
 echo $myheader->getaddheader();
+?>
 
-echo "
-        <div style='margin-top:80px;'></div>
+        <div class="mainpage" style='margin-top:80px;'>
 
-        <div class='justify-content-around py-5 px-3 mx-3 mt-5 bg-dark text-light rounded'>
+        <div class='justify-content-around py-5 px-3 mx-3 mt-5 text-light rounded-top'>
             <h3 class='pl-2'>Le Studio</h3>
             <p>Bloodless Studio c'est formé dans le cadre d'un projet de groupe. Pour notre projet de second semestre nous avions à construire ensemble un projet sur Unity et codé en C#. Nous avons ainsi déssider ensemble de crée Bloodless Studio. Studio qui va débuter sa première création en février 2022. Artificial psycho est alors en développement et toute l'équipe se donne à fond dans le projet. </p>
         </div>
 
-        <div class='mx-3 px-5 py-2 text-light bg-secondary'><h3> Les Membres </h3></div>
-        <div class='mx-3 d-flex justify-content-around bg-secondary text-light shadow-sm p-3 rounded'>
+        <div class='mx-3 px-5 py-2 text-light'><h3> Les Membres </h3></div>
+        <div class='mx-3 d-flex justify-content-around text-light shadow-sm p-3 rounded-bottom'>
             <figure class='figure'>
                 <img src='images/pp2.jpg' class='figure-img img-fluid img-thumbnail rounded' alt='...'>
                 <figcaption class='figure-caption text-center'>Hugo FRANGIAMONE</figcaption>
@@ -37,6 +37,6 @@ echo "
                 <figcaption class='figure-caption text-center'>Léonard FIGUINHA</figcaption>
             </figure>
         </div>
+        </div>
 </body>
-</html>";
-?>
+</html>
