fa24964c841494e4a3503df4b832064f24374f54
Abel AUBRON <abel.aubron@epita.fr>
2022-06-04 12:34:19 +0200

    receive data setup 3

--- data_recever.php
+++ data_recever.php
@@ -5,16 +5,17 @@ global $database;
 if (!empty($_POST))
 {
     var_dump($_POST);
-}
-var_dump($_POST);
+    $pseudo = $_POST['Name'];
+    $time = $_POST['Time'];
+    $score = $_POST['Score'];
 /*
 if (isset($_POST['Pseudo'])) {
     $data = $_POST['Pseudo'];
     echo "Test Passed, Variables : ".$data;
 }
 */
-$sql = "INSERT INTO `scoreboard` (`Name`,`Time`,`Score`,`Date`) VALUES (?,?,?,now())";
-$database->prepare($sql)->execute([$pseudo, $time, $score]);
-
+    $sql = "INSERT INTO `scoreboard` (`Name`,`Time`,`Score`,`Date`) VALUES (?,?,?,now())";
+    $database->prepare($sql)->execute([$pseudo, $time, $score]);
+}
 //$database->query("INSERT INTO `scoreboard` (`Name`,`Time`,`Score`,`Date`) VALUES ('$pseudo','$time',$score,now())");
 ?>
