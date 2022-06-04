6a6ae72fcbc2cadad15de0c1077de3cf62de3f5f
Abel AUBRON <abel.aubron@epita.fr>
2022-06-04 12:20:57 +0200

    receive data setup

--- data_recever.php
+++ data_recever.php
@@ -1,9 +1,16 @@
 <?php
+
+if (!empty($_POST))
+{
+    var_dump($_POST);
+}
+var_dump($_POST);
+/*
 if (isset($_POST['Pseudo'])) {
     $data = $_POST['Pseudo'];
     echo "Test Passed, Variables : ".$data;
 }
-echo 'Works';
+*/
 
 //$database->query("INSERT INTO `scoreboard` (`Name`,`Time`,`Score`,`Date`) VALUES ('$pseudo','$time',$score,now())");
 ?>
